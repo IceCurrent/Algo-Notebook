38a1301a33d4b493a2b460fb9f07ca70dcd72091
IceCurrent <95679948+IceCurrent@users.noreply.github.com>
2023-06-21 02:33:17 +0530

    added dijkstra's algo

---
+++ Graphs/dijkstras.cpp
@@ -0,0 +1,196 @@
+/* Header Files */
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include<unordered_map>
+#include<unordered_set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <climits>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define fo(i,n) for(int i=0;i<n;i++)
+#define F first
+#define S second
+#define pb push_back
+
+
+//remove consecutive duplicate elements from the vector
+#define uniq(v) v.erase(unique(all(v)),v.end())
+
+
+//for debugging (#converts the variable name into string literal)
+#define deb(x) cout<<#x<<"="<<x<<endl
+
+
+
+#define PI 3.1415926535897932384626433832795l 
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+// ----------------------</BITWISE>-------------------------- 
+/* a=target variable, b=bit number to act upon 0-n */
+#define BIT_SET(a,b) ((a) |= (1ULL<<(b)))
+#define BIT_CLEAR(a,b) ((a) &= ~(1ULL<<(b)))
+#define BIT_FLIP(a,b) ((a) ^= (1ULL<<(b)))
+
+// '!!' to make sure this returns 0 or 1
+#define BIT_CHECK(a,b) (!!((a) & (1ULL<<(b))))
+
+#define BITMASK_SET(x, mask) ((x) |= (mask))
+#define BITMASK_CLEAR(x, mask) ((x) &= (~(mask)))
+#define BITMASK_FLIP(x, mask) ((x) ^= (mask))
+#define BITMASK_CHECK_ALL(x, mask) (!(~(x) & (mask)))
+#define BITMASK_CHECK_ANY(x, mask) ((x) & (mask))
+// ----------------------</BITWISE END>--------------------------
+
+// ----------------------</MATH>---------------------------------
+template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);} 
+
+template<typename T> T lcm(T a, T b){return(a*(b/gcd(a,b)));} 
+
+int mul(int a, int b, int c = MOD){ll res=(ll)a*b;
+                         return(res>=c?res%c:res);}
+
+
+//simple exponentiation
+template<typename T>T expo(T e, T n){T x=1,p=e;while(n)
+                         {if(n&1)x=x*p;p=p*p;n>>=1;}return x;} 
+
+//exponentiation but the result is kept in bounds of MOD
+template<typename T>T power(T e, T n, T m = MOD){T x=1,p=e;while(n)
+                      {if(n&1)x=mul(x,p,m);p=mul(p,p,m);n>>=1;}return x;} 
+
+
+template <class T> T absmax(T a, T b)
+{return (abs(a)>abs(b)) ? a : b ;}
+
+/****************** Prime Generator **********************/ 
+
+//generate prime numbers till a
+const int N=1e7+10; int prime[20000010]; 
+bool isprime[N]; int nprime; 
+template <typename T> void Sieve(T a) 
+{nprime = 0;memset(isprime,true,sizeof(isprime));
+isprime[1]=false;for(int i=2;i<N;i++){
+if(isprime[i]){prime[nprime++]=i;for(int j=2;i*j<N;j++)
+isprime[i*j]=false;}}}
+
+
+//returns the number of prime factors of n
+template <typename T> inline T PrimeFactors(T n)
+{ll cnt=0,sum=1;for(int i=0; prime[i]*prime[i]<=n 
+and i<nprime;i++){cnt=0;while(n%prime[i]==0)
+{cnt++;n/=prime[i];}sum*=(cnt+1);}
+if(n>1)sum*=2;return sum;} 
+/****************** Prime Generator End **********************/ 
+
+typedef unordered_set<int> usi;
+typedef unordered_set<ll> usl;
+typedef unordered_set <string> uss;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pl;
+typedef pair<string,string> pss;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+
+//This is basically what we're doin':
+/*
+we make a finalised array, which contains those elements, whose
+distance from the source has been finalised. 
+
+We finalise the distance of one vertex in each iteration. That 
+vertex is basically the lowest curr distance vertex. We keep track 
+of curr distance by a distance vector, which is also updated at each
+iteration. 
+
+So two things happen in 1 iteration, the lowest curr distance vertex
+is finalised. All the non finalised vertices which are adjacent
+to the vertex just finalised are relaxed.
+*/
+
+//TIME COMPLEXITY: O(n^2); 
+vi shortestDist(vector<vi> adj, int n, int src){
+    vi dist(n+1, INT_MAX);
+    dist[src] = 0;
+
+    vb fin(n+1, false);
+
+    //note that if n-1 vertices are finalised, then automatically
+    //the last one is also finalsed, so we run the loop just n-1 times
+    for(int i=1; i<n; i++){
+        int u = -1;
+        for(int j=1; j<=n; j++){
+            if(!fin[j] &&(u == -1 || dist[j] < dist[u])){
+                u = j;
+            }
+        }   
+
+        fin[u] = true;
+
+        for(int j=1; j<=n; j++){
+            if(!fin[j] && adj[u][j]){
+                dist[j] = min(dist[j], dist[u] + adj[u][j]);
+            }
+        }
+    }
+
+    return dist;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m;
+    cin>>n>>m;
+
+    vi v(n+1, 0);
+    vector<vi> adj(n+1, v);
+
+    fo(i, m){
+        int x, y, w;
+        cin>>x>>y>>w;
+
+        adj[x][y] = w;
+        adj[y][x] = w;
+
+    }
+
+    vi dist = shortestDist(adj, n, 1);
+
+    for(int i=1; i<=n; i++){
+        cout<<dist[i]<<" ";
+    }
+    cout<<"\n";
+
+    return 0;
+}
+
