5028822b5a338ffd6450a8f60a8be264142fac32
IceCurrent <95679948+IceCurrent@users.noreply.github.com>
2023-06-24 04:44:26 +0530

    added building roads problem

---
+++ CSES/Graphs/Building_Roads.cpp
@@ -0,0 +1,171 @@
+/* Header Files */
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include<unordered_map>
+#include<unordered_set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <climits>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define fo(i,n) for(int i=0;i<n;i++)
+#define F first
+#define S second
+#define pb push_back
+
+
+//remove consecutive duplicate elements from the vector
+#define uniq(v) v.erase(unique(all(v)),v.end())
+
+
+//for debugging (#converts the variable name into string literal)
+#define deb(x) cout<<#x<<"="<<x<<endl
+
+
+
+#define PI 3.1415926535897932384626433832795l 
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+// ----------------------</BITWISE>-------------------------- 
+/* a=target variable, b=bit number to act upon 0-n */
+#define BIT_SET(a,b) ((a) |= (1ULL<<(b)))
+#define BIT_CLEAR(a,b) ((a) &= ~(1ULL<<(b)))
+#define BIT_FLIP(a,b) ((a) ^= (1ULL<<(b)))
+
+// '!!' to make sure this returns 0 or 1
+#define BIT_CHECK(a,b) (!!((a) & (1ULL<<(b))))
+
+#define BITMASK_SET(x, mask) ((x) |= (mask))
+#define BITMASK_CLEAR(x, mask) ((x) &= (~(mask)))
+#define BITMASK_FLIP(x, mask) ((x) ^= (mask))
+#define BITMASK_CHECK_ALL(x, mask) (!(~(x) & (mask)))
+#define BITMASK_CHECK_ANY(x, mask) ((x) & (mask))
+// ----------------------</BITWISE END>--------------------------
+
+// ----------------------</MATH>---------------------------------
+template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);} 
+
+template<typename T> T lcm(T a, T b){return(a*(b/gcd(a,b)));} 
+
+int mul(int a, int b, int c = MOD){ll res=(ll)a*b;
+                         return(res>=c?res%c:res);}
+
+
+//simple exponentiation
+template<typename T>T expo(T e, T n){T x=1,p=e;while(n)
+                         {if(n&1)x=x*p;p=p*p;n>>=1;}return x;} 
+
+//exponentiation but the result is kept in bounds of MOD
+template<typename T>T power(T e, T n, T m = MOD){T x=1,p=e;while(n)
+                      {if(n&1)x=mul(x,p,m);p=mul(p,p,m);n>>=1;}return x;} 
+
+
+template <class T> T absmax(T a, T b)
+{return (abs(a)>abs(b)) ? a : b ;}
+
+/****************** Prime Generator **********************/ 
+
+// //generate prime numbers till a
+// const int N=1e7+10; int prime[20000010]; 
+// bool isprime[N]; int nprime; 
+// template <typename T> void Sieve(T a) 
+// {nprime = 0;memset(isprime,true,sizeof(isprime));
+// isprime[1]=false;for(int i=2;i<N;i++){
+// if(isprime[i]){prime[nprime++]=i;for(int j=2;i*j<N;j++)
+// isprime[i*j]=false;}}}
+
+
+// //returns the number of prime factors of n
+// template <typename T> inline T PrimeFactors(T n)
+// {ll cnt=0,sum=1;for(int i=0; prime[i]*prime[i]<=n 
+// and i<nprime;i++){cnt=0;while(n%prime[i]==0)
+// {cnt++;n/=prime[i];}sum*=(cnt+1);}
+// if(n>1)sum*=2;return sum;} 
+/****************** Prime Generator End **********************/ 
+
+typedef unordered_set<int> usi;
+typedef unordered_set<ll> usl;
+typedef unordered_set <string> uss;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pl;
+typedef pair<string,string> pss;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+
+const int Max = 1e5 + 1;
+int n, m;
+bool vis[Max];
+vector<int> graph[Max];
+
+void dfs(int n)
+{
+    for (int i : graph[n])
+        if (!vis[i])
+        {
+            vis[i] = 1;
+            dfs(i);
+        }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int tem1, tem2;
+        cin >> tem1 >> tem2;
+        graph[tem1].push_back(tem2);
+        graph[tem2].push_back(tem1);
+    }
+
+    int cnt = -1;
+    vector<int> roads;
+    for (int i = 1; i <= n; i++)
+    {
+        if (!vis[i])
+        {
+            roads.push_back(i);
+            dfs(i);
+            cnt++;
+        }
+    }
+
+    cout << cnt << '\n';
+    for (int i = 0; (size_t)i < roads.size() - 1; i++)
+        cout << roads[i] << ' ' << roads[i + 1] << '\n';
+
+    return 0;
+}
+
